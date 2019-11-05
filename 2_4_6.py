47c950a54f5f8819ff9d426aa4807c31c3b5908f
Alexandr Letov <AlexandrLetov2018@yandex.ru>
2019-11-05 12:54:52 +0300

    Add 2_4_6.py

--- 2_4_6.py
+++ 2_4_6.py
@@ -1,5 +1,12 @@
 import os
-import os.path
 
-print(os.getcwd())
-print(os.listdir())
+result = []
+os.chdir('./Data/')
+for current_dir, dirs, files in os.walk('.'):
+    for file in files:
+        if file[-3:] == '.py':
+            if current_dir[2:] not in result:
+                result.append(current_dir[2:])
+
+for _dir in sorted(result)
+    print(_dir)
